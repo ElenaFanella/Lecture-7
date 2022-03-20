1a6c5607d122ec9a1341607e3644fee28ecb2a86
Elena Fanella <101728948+ElenaFanella@users.noreply.github.com>
2022-03-20 17:42:36 +0000

    Correzione

--- main.py
+++ main.py
@@ -1,82 +1,3 @@
-print("-------ESERCIZIO 6-------- \n la risposta teorica è un commento nel codice")
-
-"""
-Le funzioni lambda sono funzioni senza nome, che svolgono operazioni semplici definite in un'unica linea di comando, e restituiscono un unico valore.
-
-- lambda a: a+10 
-incrementa di 10 unità il valore di a.
-Esempio:
-f= lambda a: a+10
-print(f(2))
-Risutato: 12
-
--f = lambda animale: animale.capitalize()
-f è una funzione che rende maiuscola l'iniziale di una stringa di caratteri.
-Esempio:
-animali = ['cani', 'gatti', 'scoiattoli', 'alci']
-f = lambda animale: animale.capitalize()
-for animale in animali:
- print(f(animale))
-Risultato:
-Cani 
-Gatti
-Scoiattoli
-Alci
-"""
-
-f= lambda a,b: (a+b)*0.5
-media=f(5.0,0.7)
-print(media)
-
-
-print("\n\n-------ESERCIZIO 5--------")
-def dotpro(l1,l2):
-  if len(l1)!=len(l2):
-    raise TypeError("Le due liste devono avere stessa lunghezza")
-  else:
-    pro=0
-    for i in range(0, len(l1)):
-      pro=pro+l1[i]*l2[i]
-  return pro
-
-
-def matrix_product(X, Y):
-   if len(X[0])!= len(Y) :
-     raise NoneType
-     print("Le matrici devono essere di dimensioni MXN NXP")
-   else:
-     result = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
-     for i in range(len(X)):
-      for j in range(len(Y[0])):
-        for k in range(len(Y)):
-          result[i][j] += X[i][k] * Y[k][j]
-     return result
-
-
-
-list1=[2.0,4.5,7.2,6.2]
-list2=[0.5,7.5,2.4,7.0]
-list3=[6.3,4.0,4.0]
-A = [[12-0,7.3,3.5],[4.4 ,5.3,6.4],[7.4 ,8.3,9.3]]
-B = [[5.3,8.6,1.2,2.4],[6.4,7.1,3.2,0.45],[4.3,5.3,9.3,1.3]]
-
-dp=dotpro(list1,list2)
-#dp2=dotpro(list1,list3)
-mp=matrix_product(A,B)
-
-def print_matrix(matrix):
-  for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-      print(str((matrix[i][j]))+"\t", end='')
-    print("\n")
-
-print("prodotto scalare",dp)
-print("matrice prodotto")
-print_matrix(mp)
-
-
-
-
 print("\n\n ----ESERCIZIO 1----")
 import math
 class point3D:
@@ -115,10 +36,17 @@ righe=indata.readlines()
 indata.close()
 M1=[]
 M2=[]
+s=0
 for el in righe:
-  valori=el.split()
-  M1.append(float(valori[0]))
-  M2.append(float(valori[1]))
+ s=s+1
+ valori=el.split()
+ try:
+   M1.append(float(valori[0]))
+   M2.append(float(valori[1]))
+ except ValueError:
+   print("Ho saltato la riga" ,s,"perché non vi è una coppia di numeri")
+ 
+ 
 print(M1)
 print(M2)
 
@@ -169,10 +97,15 @@ righe=indata.readlines()
 indata.close()
 Xi=[]
 Yi=[]
+s=0
 for el in righe:
-  valori=el.split()
-  Xi.append(float(valori[0]))
-  Yi.append(float(valori[1]))
+ s=s+1
+ valori=el.split()
+ try:
+   Xi.append(float(valori[0]))
+   Yi.append(float(valori[1]))
+ except ValueError:
+   print("Ho saltato la riga" ,s,"perché non vi è una coppia di numeri")
 print(Xi)
 print(Yi)
 
@@ -238,7 +171,7 @@ class campionato_sportivo:
    last3_score=min(scores_new2)
    last3_index=scores_new2.index(min(scores_new2))
    last3_name=names_new2[last3_index]
-   print( "Ultime classificate: \n 1)", self.squad[last_index], self.score[last_index],  "\n2)",self.squad[last2_index],self.score[last2_index], "\n3)",self.squad[last3_index],self.score[last3_index])
+   print( "Ultime classificate: \n", n-2, "posto", last3_name,last3_score, "\n", n-1, "posto:",last2_name,last2_score, "\n",n,"posto e ultimo:",last_name, last_score,)
      
          
 import sys
@@ -252,6 +185,8 @@ scores=[]
 for i in range(n): 
   print("Inserisci il nome della squadra",i)
   squadra=input()
+  while squadra in names:
+   squadra=input("Questa squadra sta già giocando, scegli un altro nome \n")
   names.append(squadra)
   scores.append(random.randint(0,70))
   
@@ -262,3 +197,80 @@ campionato.squalifica()
 campionato.winner()
 campionato.retrocessione()
 
+
+
+print("\n\n-------ESERCIZIO 5--------")
+def dotpro(l1,l2):
+  if len(l1)!=len(l2):
+    raise TypeError("Le due liste devono avere stessa lunghezza")
+  else:
+    pro=0
+    for i in range(0, len(l1)):
+      pro=pro+l1[i]*l2[i]
+  return pro
+
+
+def matrix_product(X, Y):
+   if len(X[0])!= len(Y) :
+     raise NoneType
+     print("Le matrici devono essere di dimensioni MXN NXP")
+   else:
+     result = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
+     for i in range(len(X)):
+      for j in range(len(Y[0])):
+        for k in range(len(Y)):
+          result[i][j] += X[i][k] * Y[k][j]
+     return result
+
+
+
+list1=[2.0,4.5,7.2,6.2]
+list2=[0.5,7.5,2.4,7.0]
+list3=[6.3,4.0,4.0]
+A = [[12-0,7.3,3.5],[4.4 ,5.3,6.4],[7.4 ,8.3,9.3]]
+B = [[5.3,8.6,1.2,2.4],[6.4,7.1,3.2,0.45],[4.3,5.3,9.3,1.3]]
+
+dp=dotpro(list1,list2)
+#dp2=dotpro(list1,list3)
+mp=matrix_product(A,B)
+
+def print_matrix(matrix):
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      print(str((matrix[i][j]))+"\t", end='')
+    print("\n")
+
+print("prodotto scalare",dp)
+print("matrice prodotto")
+print_matrix(mp)
+
+
+print("-------ESERCIZIO 6-------- \n la risposta teorica è un commento nel codice")
+
+"""
+Le funzioni lambda sono funzioni senza nome, che svolgono operazioni semplici definite in un'unica linea di comando, e restituiscono un unico valore.
+
+- lambda a: a+10 
+incrementa di 10 unità il valore di a.
+Esempio:
+f= lambda a: a+10
+print(f(2))
+Risutato: 12
+
+-f = lambda animale: animale.capitalize()
+f è una funzione che rende maiuscola l'iniziale di una stringa di caratteri.
+Esempio:
+animali = ['cani', 'gatti', 'scoiattoli', 'alci']
+f = lambda animale: animale.capitalize()
+for animale in animali:
+ print(f(animale))
+Risultato:
+Cani 
+Gatti
+Scoiattoli
+Alci
+"""
+
+f= lambda a,b: (a+b)*0.5
+media=f(5.0,0.7)
+print(media)
